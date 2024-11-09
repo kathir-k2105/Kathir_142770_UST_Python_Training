58a00a94396a207b2ac7170f7b47dce24716b377
kathir-k2105 <kathir.kumar2105@gmail.com>
2024-11-09 16:23:29 +0530

    Delete Milestone 1.py

--- Milestone 1.py
+++
@@ -1,3 +0,0 @@
-'''Milestone 1 : Create a Program to read a csv file which contains large text of some novel. 
-Get the frequencies of all words in the file and finally show the frequencies for each word in a pandas data frame.
-''''
